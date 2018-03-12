7e34498589e0a74e5e7a4c17680aa681bc782aa0
Yousef Sultan <yousef.su.2000@gmail.com>
2018-03-12 05:43:25 +0200

    Upgrade to use new library version

--- bot.js
+++ bot.js
@@ -1,33 +1,51 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
 const codebottle = require('codebottle');
 
 const bot = new Discord.Client();
-const token = '<token>';
-var prefix = '!';
+const token = process.env.TOKEN;
+const prefix = process.env.PREFIX;
 
 bot.on('ready', () => {
 	console.log(`Logged in as ${bot.user.username}!`);
 });
 
 bot.on('message', msg => {
+	console.log(`RECV: ${msg.author.tag}@${msg.channel.name} ${msg.content}`);
+
 	if (msg.content.startsWith(prefix)) {
-		if (msg.content.startsWith(prefix + 'ping')) {
-			msg.channel.sendMessage('Pong!');
-		} else if (msg.content.startsWith(prefix + 'search')) {
-			var terms = msg.content.substr(msg.content.indexOf(' ') + 1);
-			codebottle.search(terms, (data, error) => {
-				console.log('=> Search: ' + terms);
-				if (error) {
-					console.log(error);
-				} else if (data) {
+		const command = msg.content.split(' ')[0].substring(1).toLowerCase().trim();
+		const rest = msg.content.substring(command.length + 2); // 1 for prefix + 1 for trailing space
+
+		switch(command) {
+			case 'search':
+				console.log(`=> Search: ${rest}`);
+				codebottle.search(rest).then(response => {
 					const embed = new Discord.RichEmbed();
-					embed.setTitle(data[0]['title']);
-					embed.setColor('#4caf50');
-					embed.setDescription(data[0]['description']);
-					embed.setURL('https://codebottle.io/code/' + data[0]['code_id'] + '/');
-					msg.channel.sendEmbed(embed);	
-				}
-			})
+					embed.setTitle(response.data[0].title);
+					embed.setColor('#272727');
+					embed.setDescription(response.data[0].description);
+					embed.setURL(`https://codebottle.io/s/${response.data[0].id}`);
+					embed.setAuthor(response.data[0].username);
+					msg.channel.send(embed);
+				}).catch(e => {
+					msg.react('⛔');
+				});
+				break;
+			case 'get':
+				console.log(`=> Get: ${rest}`);
+				codebottle.get(rest).then(response => {
+					const embed = new Discord.RichEmbed();
+					embed.setTitle(response.data.title);
+					embed.setColor('#272727');
+					embed.setDescription(response.data.description);
+					embed.setURL(`https://codebottle.io/s/${response.data.id}`);
+					embed.setAuthor(response.data.username);
+					msg.channel.send(embed);
+				}).catch(e => {
+					msg.react('⛔');
+				});
 		}
 	}
 })
