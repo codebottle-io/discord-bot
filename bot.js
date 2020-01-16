9b8388e6c0d8af9d4611947ba6c16e8451a35d63
NightScript <maorninja@outlook.com>
2020-01-15 21:53:20 -0800

    Code touchups

--- bot.js
+++ bot.js
@@ -7,61 +7,50 @@ const bot = new Discord.Client();
 const token = process.env.TOKEN;
 const prefix = process.env.PREFIX;
 
-bot.on('ready', () => {
-	console.log(`Logged in as ${bot.user.username}!`);
-});
-
+bot.on('ready', () => console.log(`Logged in as ${bot.user.username}!`));
 bot.on('message', msg => {
-	console.log(`RECV: ${msg.author.tag}@${msg.channel.name} ${msg.content}`);
+	if (!msg.content.startsWith(prefix) || (msg.guild && !msg.channel.permissionsFor(bot.client.user).has('SEND_MESSAGES')))
+		return;
 
-	if (msg.content.startsWith(prefix)) {
-		const command = msg.content.split(' ')[0].substring(1).toLowerCase().trim();
-		const rest = msg.content.substring(command.length + 2); // 1 for prefix + 1 for trailing space
+	const command = msg.content.split(' ')[0].substring(1).toLowerCase().trim();
+	const rest = msg.content.substring(command.length + 2); // 1 for prefix + 1 for trailing space
 
-		switch(command) {
-			case 'search':
-				console.log(`=> Search: ${rest}`);
-				codebottle.search(rest).then(response => {
-					const description = response[0].description ? response[0].description
-						: '*No description provided.*';
-					const embed = new Discord.RichEmbed();
-					embed.setTitle(response[0].title);
-					embed.setColor('#272727');
-					embed.setDescription(response[0].description);
-					embed.setURL(`https://codebottle.io/s/${response[0].id}`);
-					embed.setAuthor(response[0].username);
-					msg.channel.send(embed);
-				}).catch(e => {
-					msg.react('⛔');
-				});
-				break;
-			case 'get':
-				console.log(`=> Get: ${rest}`);
-				codebottle.get(rest).then(response => {
-					const description = response.description ? response.description
-						: '*No description provided.*';
-					const embed = new Discord.RichEmbed();
-					embed.setTitle(response.title);
-					embed.setColor('#272727');
-					embed.setDescription(response.description);
-					embed.setURL(`https://codebottle.io/s/${response.id}`);
-					embed.setAuthor(response.username);
-					msg.channel.send(embed);
-				}).catch(e => {
-					msg.react('⛔');
-				});
-				break;
-			case 'help':
-				const embed = new Discord.RichEmbed();
-				embed.setTitle('CodeBottle');
-				embed.setColor('#272727');
-				embed.setDescription(`
+	switch(command) {
+		case 'search':
+			codebottle.search(rest).then(response => {
+				const description = response[0].description ? response[0].description : '*No description provided.*';
+				const embed = new Discord.RichEmbed()
+					.setTitle(response[0].title)
+					.setColor('#272727')
+					.setDescription(description)
+					.setURL(`https://codebottle.io/s/${response[0].id}`)
+					.setAuthor(response[0].username);
+				msg.channel.send(embed);
+			}).catch(e => msg.react('⛔'));
+			break;
+		case 'get':
+			codebottle.get(rest).then(response => {
+				const description = response.description ? response.description : '*No description provided.*';
+				const embed = new Discord.RichEmbed()
+					.setTitle(response.title)
+					.setColor('#272727')
+					.setDescription(response.description)
+					.setURL(`https://codebottle.io/s/${response.id}`)
+					.setAuthor(response.username);
+				msg.channel.send(embed);
+			}).catch(e => msg.react('⛔'));
+			break;
+		case 'help':
+			const embed = new Discord.RichEmbed()
+				.setTitle('CodeBottle')
+				.setColor('#272727')
+				.setDescription(`
 This is a bot for https://codebottle.io/
 
-\`!search <keywords>\` Search snippets with keywords
-\`!get <id>\` Get a snippet by its ID`);
-				msg.channel.send(embed);
-				break;
+\`!search <keywords>\` - Search snippets with keywords
+\`!get <id>\` - Get a snippet by its ID`);
+			msg.channel.send(embed);
+			break;
 		}
 	}
 })
